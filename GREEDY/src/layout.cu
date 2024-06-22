8b0d58f0b63fc3efba5003e358af6f80927fa202
Frederik Jatzkowski <f.l.jatzkowski@gmail.com>
2024-06-22 23:56:34 +0200

    changed output to row-major list of barcodes

--- GREEDY/src/layout.cu
+++ GREEDY/src/layout.cu
@@ -8,7 +8,7 @@
 #include <stdio.h>
 
 namespace layout {
-    void print(FILE *target, const barcodes::Set *barcode_set, const struct Layout *layout) {
+    void json(FILE *target, const barcodes::Set *barcode_set, const struct Layout *layout) {
         fputs("[\n", target);
         for (size_t y = 0; y < COL_COUNT; y++) {
             fputs("\t[", target);
@@ -35,6 +35,19 @@ namespace layout {
         }
 
         fputs("]\n", target);
+    }
+    void print(FILE *target, const barcodes::Set *barcode_set, const struct Layout *layout) {
+        for (size_t x = 0; x < ROW_COUNT; x++) {
+            for (size_t y = 0; y < COL_COUNT; y++) {
+                const struct Position *position = &layout->positions[x][y];
+                const barcodes::Barcode *barcode = &(*barcode_set)[position->i_barcode];
+
+                for (size_t i = 0; i < BARCODE_LENGTH; i++)
+                    fputc(barcode->nucleotides[i], target);
+                
+                fputs("\n", target);
+            }
+        }
     }
 }
 
